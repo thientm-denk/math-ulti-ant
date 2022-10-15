d239b0fb0585579548e4e4ebea543968f22fd64f
Denkhotieu <denkhotieu@gmail.com>
2022-10-15 15:56:05 +0700

    UpdateJDKFile

--- src/com/denk/mathutil/main/Main.java
+++ src/com/denk/mathutil/main/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This Messge come form main");
         System.out.println("This Jar file is built base on Ant co-operatin with JUnit");
+        System.out.println("Mỏe OVer, Thí messeage comes form a US-BUILT JAR FILE");
         tryTDDFrist();
 
     }
